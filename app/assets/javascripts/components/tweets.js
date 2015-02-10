3c1e6e4bfa333c4abcffeed664615953f1c4265b
Anthony Ng & Leonard Bui <pair+anthony-ng.lnbcoding@devbootcamp.com>
2015-02-10 15:54:08 -0800

    add ajax call to get tweets from api call and rendering

--- app/assets/javascripts/components/tweets.js
+++ app/assets/javascripts/components/tweets.js
@@ -1,7 +1,52 @@
 //= require react
-
 //= require components/tweet
+
 var Tweets = React.createClass({displayName: "Tweets",
+  // 1st: initialize the tweets array----------------------------------
+  getInitialState: function() {
+    return {
+      tweets: []
+    };
+  },
+
+  // 2nd: set a property of state with a named tweets------------------
+
+  // call server and update the state
+  // componentDidMount is a built-in React method
+  componentDidMount: function() {
+    $.ajax({
+      url: '/tweets/recent',
+      type: 'GET'
+    }).done(function(serverResponse) {
+  // setting a State name tweets. setState is a built-in React method
+      this.setState({
+        tweets: serverResponse
+      })
+    }.bind(this))
+  },
+
+  // 4th: accept the ajax response and convert to proper format for rendering
+  renderTweets: function() {
+    // var data = [{id: 1,
+    //             avatar_url: "http://robohash.org/abc",
+    //             content: "abc",
+    //             created_at: "2014-07-25T23:04:13Z",
+    //             handle: "eee",
+    //             hashtag_names: ["deleniti"],
+    //             updated_at: "2014-07-25T23:04:13Z",
+    //             username: "abc"}]
+
+    // the var data is hard coded
+    // var tweets = data.map(function(tweet){
+
+    var tweets = this.state.tweets.map(function(tweet){
+      return(React.createElement(Tweet, {key: tweet.id, tweet: tweet}));
+    })
+    return tweets;
+  },
+
+  // 3rd: render to the <Tweets /> in twitter.jsx
+
   render: function() {
     return (
       React.createElement("section", {id: "tweets-container"}, 
@@ -11,20 +56,6 @@ var Tweets = React.createClass({displayName: "Tweets",
         )
       )
     )
-  },
-  renderTweets: function() {
-    var data = [{id: 1,
-                avatar_url: "http://robohash.org/abc",
-                content: "abc",
-                created_at: "2014-07-25T23:04:13Z",
-                handle: "eee",
-                hashtag_names: ["deleniti"],
-                updated_at: "2014-07-25T23:04:13Z",
-                username: "abc"}]
-
-    var tweets = data.map(function(tweet){
-      return(React.createElement(Tweet, {key: tweet.id, tweet: tweet}));
-    })
-    return tweets;
   }
+
 })
